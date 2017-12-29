a5b10c6cb1734f4e6432f91ab95cf9c9a2d32a36
Daniel Muey <simplemood.com@gmail.com>
2017-12-29 13:27:47 -0600

    update 0.39 for release

--- lib/File/Copy/Recursive.pm
+++ lib/File/Copy/Recursive.pm
@@ -23,7 +23,7 @@ require Exporter;
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(fcopy rcopy dircopy fmove rmove dirmove pathmk pathrm pathempty pathrmdir rcopy_glob rmove_glob);
 
-$VERSION = '0.38';
+$VERSION = '0.39';
 
 $MaxDepth = 0;
 $KeepMode = 1;
